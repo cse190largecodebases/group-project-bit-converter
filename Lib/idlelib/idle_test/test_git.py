10451c7721090c8042d38057ef2baf0bf01e66bf
Kenneth Casimiro <kennethreyescasimiro@gmail.com>
2023-05-30 06:02:10 -0700

    Added comment for repo path

--- Lib/idlelib/idle_test/test_git.py
+++ Lib/idlelib/idle_test/test_git.py
@@ -6,11 +6,14 @@ class TestSourceControlGUI(unittest.TestCase):
     def test_git_status(self):
         root = Tk()
         app = SourceControlGUI()
+
+        # Will need to change to a working repo that has git initialized
         app.perform_repo("/Users/ken/Desktop/group-project-bit-converter")
+        
         repo_status = app.repo.git.status()
         expected_phrase = "Your branch is up to date with"
         self.assertIn(expected_phrase, repo_status)
-        
+
         # Cleanup
         root.destroy()
 
