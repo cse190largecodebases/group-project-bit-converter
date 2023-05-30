cb7d6ba97a2924f11797d654fea087d20eee1972
Kenneth Casimiro <kennethreyescasimiro@gmail.com>
2023-05-30 06:01:27 -0700

    Completed Unit Test for git status

---
+++ Lib/idlelib/idle_test/test_git.py
@@ -0,0 +1,19 @@
+import unittest
+from tkinter import Tk
+from idlelib.git import SourceControlGUI
+
+class TestSourceControlGUI(unittest.TestCase):
+    def test_git_status(self):
+        root = Tk()
+        app = SourceControlGUI()
+        app.perform_repo("/Users/ken/Desktop/group-project-bit-converter")
+        repo_status = app.repo.git.status()
+        expected_phrase = "Your branch is up to date with"
+        self.assertIn(expected_phrase, repo_status)
+        
+        # Cleanup
+        root.destroy()
+
+
+if __name__ == '__main__':
+    unittest.main()
